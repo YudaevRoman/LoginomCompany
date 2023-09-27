4f2711167857d99436e3653699ef8471f7acdaca
Leere Ewigkeit <ewileere@gmail.com>
2023-09-27 03:08:40 +0300

    Add constructor in Attribute class

--- Transaction/Attribute.cs
+++ Transaction/Attribute.cs
@@ -4,4 +4,10 @@ public class Attribute : IAttribute<int, string>
 {
     public int Key { get; set; }
     public string Value { get; set; }
+
+    Attribute(int key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
 }
